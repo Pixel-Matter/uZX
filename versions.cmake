1ea8575b4ea9b8e63ac365d14a7762be6ac7cee7
Ru Guevara <ruslan.gr@gmail.com>
2026-03-05 09:16:05 +0100

    version: bump to 0.4.2

--- versions.cmake
+++ versions.cmake
@@ -1,5 +1,5 @@
 # Core (shared engine/library) version
-set(PROJECT_CORE_VERSION "0.4.1")
+set(PROJECT_CORE_VERSION "0.4.2")
 
 # Per-target application versions
 set(UZX_STUDIO_VERSION "${PROJECT_CORE_VERSION}")
